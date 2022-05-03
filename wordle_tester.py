7b3795f0d04e6888c1f6293b5f4d0d1550ee593d
Cynopolis <Cynopolis@users.noreply.github.com>
2022-05-02 22:18:12 -0500

    Started creating a tester for the wordle solver

--- wordle_tester.py
+++ wordle_tester.py
@@ -1,15 +1,10 @@
 import wordle_solver
 
-word = "irate"
-not_letters = ""
-has_letters = ""
-position_letters = "_____"
-
 def wordle_guess(not_letters, has_letters, position_letters):
     input_file_path = 'cleaned_words.txt'
     dictionary = open(input_file_path, 'r')
-
-    guesses = wordle_solver.words_without_letters(not_letters, dictionary)
+    guesses = wordle_solver.read_in_dict(dictionary)
+    guesses = wordle_solver.words_without_letters(not_letters, guesses)
     guesses = wordle_solver.words_with_letters(has_letters, guesses)
     guesses = wordle_solver.words_with_letter_positions(position_letters, guesses)
 
@@ -19,3 +14,25 @@ def wordle_guess(not_letters, has_letters, position_letters):
 
     return guesses
 
+def check_guess(guess, word, not_letters, has_letters, position_letters):
+
+    for i, letter in enumerate(guess):
+        if letter in word:
+            if letter not in has_letters:
+                if letter == word[i]:
+                    position_letters = position_letters[:i] + letter + position_letters[i+1:]
+            has_letters += letter
+        else:
+            not_letters += letter
+    
+    return not_letters, has_letters, position_letters
+
+word = "irate"
+not_letters = ""
+has_letters = ""
+position_letters = "_____"
+
+for i in range(10):
+    guess = wordle_guess(not_letters, has_letters, position_letters)
+    not_letters, has_letters, position_letters = check_guess(guess[0], word, not_letters, has_letters, position_letters)
+    print(guess[0], not_letters, has_letters, position_letters)
